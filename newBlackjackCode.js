c9a7ec7362b7b208cdc6a3d22535e4f3564bd0d0
Gecse Gergő <gecsegergo006@gmail.com>
2025-05-30 22:46:37 +0200

    Start button, button enable function, Completed dealing

--- newBlackjackCode.js
+++ newBlackjackCode.js
@@ -1,21 +1,44 @@
+let typeOfCard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-let typeOfCard = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let deck = [typeOfCard, typeOfCard, typeOfCard, typeOfCard];
 
-let deck = [typeOfCard,typeOfCard,typeOfCard,typeOfCard]
+let hitButton = document.getElementById("hit-game-button");
+let standButton = document.getElementById("stand-game-button");
+let startButton = document.getElementById("start");
 
+standButton.style.display = "none";
+hitButton.style.display = "none";
 
-let hitButton = document.getElementById("hit-game-button")
-let standButton = document.getElementById("stand-game-button")
+let dealer = [];
+let player = [];
+let stand = false;
 
-
-function Dealing (gameDeck)
-{
-    let elso = parseInt(Math.random(0,4));
-    let masodik = parseInt(Math.floor(Math.random() * 15))
-    return gameDeck[elso][masodik];
-
+function EnableButtons(){
+  standButton.style.display = "inline-block";
+  hitButton.style.display = "inline-block";
 }
 
-console.log(Dealing(deck));
+function Dealing() {
+  let elso = parseInt(Math.random(0, 4));
+  let masodik = parseInt(Math.floor(Math.random() * 15));
 
+  let szam = deck[elso][masodik];
 
+  deck[elso][masodik] = 0;
+  return szam;
+}
+
+function Round() {
+  standButton.style.display = "none";
+  hitButton.style.display = "none";
+  dealer[dealer.length] = Dealing();
+  player[player.length] = Dealing();
+  setTimeout()
+}
+Round();
+console.log(dealer);
+console.log(player);
+
+startButton.addEventListener("click", () => {
+  EnableButtons()
+});
