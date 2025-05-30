4d74c60cbd2d3cbd7b88a8ebc50695e7b612d50e
GGergo <gecsegergo006@gmail.com>
2025-05-30 14:26:24 +0200

    Dealing

--- newBlackjackCode.js
+++ newBlackjackCode.js
@@ -1,9 +1,21 @@
-let deck = [[[]],[[]],[[]],[[]]]
 
-deck.forEach(k => {
-    for (let i = 0; i < 15; i++)
-    {
-        k[i] = true;
-        console.log(k[i])
-    }
-})
+let typeOfCard = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+let deck = [typeOfCard,typeOfCard,typeOfCard,typeOfCard]
+
+
+let hitButton = document.getElementById("hit-game-button")
+let standButton = document.getElementById("stand-game-button")
+
+
+function Dealing (gameDeck)
+{
+    let elso = parseInt(Math.random(0,4));
+    let masodik = parseInt(Math.floor(Math.random() * 15))
+    return gameDeck[elso][masodik];
+
+}
+
+console.log(Dealing(deck));
+
+
