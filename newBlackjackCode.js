79ddcbc0251293d05de01e2683b1fba92500a56d
Gecse Gergő <gecsegergo006@gmail.com>
2025-05-31 10:38:10 +0200

    Dealing properly, checks if deck is empty

--- newBlackjackCode.js
+++ newBlackjackCode.js
@@ -1,6 +1,9 @@
-let typeOfCard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// let typeOfCard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-let deck = [typeOfCard, typeOfCard, typeOfCard, typeOfCard];
+// let deck = [typeOfCard, typeOfCard, typeOfCard, typeOfCard];
+let deck = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+
+console.log(deck);
 
 let hitButton = document.getElementById("hit-game-button");
 let standButton = document.getElementById("stand-game-button");
@@ -13,32 +16,73 @@ let dealer = [];
 let player = [];
 let stand = false;
 
-function EnableButtons(){
+function EnableButtons() {
   standButton.style.display = "inline-block";
   hitButton.style.display = "inline-block";
 }
 
-function Dealing() {
-  let elso = parseInt(Math.random(0, 4));
-  let masodik = parseInt(Math.floor(Math.random() * 15));
+// function DealingV1() {
+//   let elso = 0
+//   let masodik = 0
 
-  let szam = deck[elso][masodik];
+//   do {
+//     elso = parseInt(Math.floor(Math.random()*4));
+//     masodik = parseInt(Math.floor(Math.random() * 15));
+//   }while (deck[elso][masodik] < 1)
 
-  deck[elso][masodik] = 0;
-  return szam;
+//   let szam = deck[elso][masodik];
+
+//   deck[elso][masodik] = 0;
+//   return szam;
+// }
+
+function CheckDeck(k) {
+  return k > 0;
 }
 
+function DealingV2() {
+  let kartya = parseInt(Math.floor(Math.random() * 14));
+  while (deck[kartya] < 1) {
+    kartya = parseInt(Math.floor(Math.random() * 14));
+  }
+  deck[kartya] -= 1;
+
+  switch (kartya) {
+    case 10:
+      return "J";
+      break;
+    case 11:
+      return "Q";
+      break;
+    case 12:
+      return "K";
+      break;
+    case 13:
+      return "A";
+      break;
+    default:
+      return kartya + 1;
+  }
+}
 function Round() {
   standButton.style.display = "none";
   hitButton.style.display = "none";
-  dealer[dealer.length] = Dealing();
-  player[player.length] = Dealing();
-  setTimeout()
+  if (deck.some(CheckDeck)) {
+    dealer[dealer.length] = DealingV2();
+    player[player.length] = DealingV2();
+  }
+  else {
+    console.log("Üres a pakli")
+  }
+  setTimeout(EnableButtons(), 2000);
+  console.log(dealer);
+  console.log(player);
+  console.log(deck);
 }
-Round();
-console.log(dealer);
-console.log(player);
 
 startButton.addEventListener("click", () => {
-  EnableButtons()
+  EnableButtons();
+});
+hitButton.addEventListener("click", () => {
+  Round();
 });
