678411a29824387c16a25fae29d4a5d579df776f
Gecse Gergő <gecsegergo006@gmail.com>
2025-05-31 23:39:36 +0200

    playerValue

--- newBlackjackCode.js
+++ newBlackjackCode.js
@@ -4,7 +4,6 @@
 let deck = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
 
 console.log(deck);
-let action = false;
 let deckCOunt = 56;
 const hitButton = document.getElementById("hit-game-button");
 const standButton = document.getElementById("stand-game-button");
@@ -12,6 +11,8 @@ const deckDiv = document.getElementById("deck");
 const startButton = document.getElementById("start");
 const playerDiv = document.getElementById("player");
 const dealerDiv = document.getElementById("dealer");
+let playerValue = 0;
+let dealerValue = 0;
 
 standButton.style.display = "none";
 hitButton.style.display = "none";
@@ -19,6 +20,25 @@ hitButton.style.display = "none";
 let dealer = [];
 let player = [];
 let stand = false;
+
+function CardValue(c) {
+  switch (c) {
+    case "J":
+      return 10;
+      break;
+    case "Q":
+      return 10;
+      break;
+    case "Q":
+      return 10;
+      break;
+    case "A":
+      return 10;
+      break;
+    default:
+      return c;
+  }
+}
 
 function EnableButtons() {
   standButton.style.display = "inline-block";
@@ -64,51 +84,45 @@ function DealingV2() {
       return kartya + 1;
   }
 
-//egy kör functionje
+  //egy kör functionje
 }
 function Round() {
-  action = true
-  hitButton.setAttribute("disabled","true")
+  hitButton.setAttribute("disabled", "true");
   standButton.style.display = "none";
   hitButton.style.display = "none";
   if (deck.some(CheckDeck)) {
     dealer[dealer.length] = DealingV2();
-    dealerDiv.innerHTML +=  `<div>${dealer.length == 1 ? dealer[dealer.length-1] : ""}</div>`
+    dealerDiv.innerHTML += `<div>${
+      dealer.length == 1 ? dealer[dealer.length - 1] : ""
+    }</div>`;
     // Wait(1);
     console.log(dealer);
     player[player.length] = DealingV2();
-    playerDiv.innerHTML +=  `<div>${player[player.length-1]}</div>`
+    playerDiv.innerHTML += `<div>${player[player.length - 1]}</div>`;
     // Wait(1);
     console.log(player);
-    deckCOunt -= 2
+    deckCOunt -= 2;
+    playerValue = CardValue(player.length - 1);
+    dealerValue = CardValue(dealer.length - 1);
   } else {
     console.log("Üres a pakli");
   }
-  
-    standButton.style.display = "inline-block";
+
+  standButton.style.display = "inline-block";
   hitButton.style.display = "inline-block";
-  hitButton.removeAttribute("disabled","true")
-    
+  hitButton.removeAttribute("disabled", "true");
+
   deckDiv.innerHTML = deckCOunt;
   console.log(deck);
-
-  action = false
 }
-
 
 //start
 startButton.addEventListener("click", () => {
-  startButton.style.display = "none"
+  startButton.style.display = "none";
   EnableButtons();
 });
 
 //hit
 hitButton.addEventListener("click", () => {
-
-  if (!action)
-  {
-    Round()
-    
-  }
-
+  Round();
 });
