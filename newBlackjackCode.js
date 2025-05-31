c0364f6c4f2b7fd5765f2a9e2f7b36ee41015799
Gecse Gergő <gecsegergo006@gmail.com>
2025-05-31 21:00:55 +0200

    wait, comments

--- newBlackjackCode.js
+++ newBlackjackCode.js
@@ -21,25 +21,21 @@ function EnableButtons() {
   hitButton.style.display = "inline-block";
 }
 
-// function DealingV1() {
-//   let elso = 0
-//   let masodik = 0
-
-//   do {
-//     elso = parseInt(Math.floor(Math.random()*4));
-//     masodik = parseInt(Math.floor(Math.random() * 15));
-//   }while (deck[elso][masodik] < 1)
-
-//   let szam = deck[elso][masodik];
-
-//   deck[elso][masodik] = 0;
-//   return szam;
-// }
-
+//funcion a pakli elllenőrézéshez
 function CheckDeck(k) {
   return k > 0;
 }
 
+//function a késleltetéshez
+function Wait(sec) {
+  let date1 = new Date().getTime();
+  let date2 = date1 + sec * 1000;
+  while (date1 < date2) {
+    date1 = new Date().getTime();
+  }
+}
+
+//Kártya osztás egy embernek
 function DealingV2() {
   let kartya = parseInt(Math.floor(Math.random() * 14));
   while (deck[kartya] < 1) {
@@ -63,26 +59,38 @@ function DealingV2() {
     default:
       return kartya + 1;
   }
+
+//egy kör functionje
 }
 function Round() {
   standButton.style.display = "none";
   hitButton.style.display = "none";
   if (deck.some(CheckDeck)) {
     dealer[dealer.length] = DealingV2();
+    Wait(1);
+    console.log(dealer);
     player[player.length] = DealingV2();
+    Wait(1);
+    console.log(player);
+    
+  } else {
+    console.log("Üres a pakli");
   }
-  else {
-    console.log("Üres a pakli")
-  }
-  setTimeout(EnableButtons(), 2000);
-  console.log(dealer);
-  console.log(player);
+  
+    standButton.style.display = "inline-block";
+  hitButton.style.display = "inline-block";
+  
+  
   console.log(deck);
 }
 
+
+//start
 startButton.addEventListener("click", () => {
   EnableButtons();
 });
+
+//hit
 hitButton.addEventListener("click", () => {
   Round();
 });
